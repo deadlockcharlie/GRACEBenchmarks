09e1d495d16e193e6d6292a3b39933ae2f17d825
Ayush Pandey <ayushpandey8439@gmail.com>
2025-09-13 19:52:23 +0200

    Create single replica deployments

--- Deployments/MongoDBReplicatedDeployment.sh
+++ Deployments/MongoDBReplicatedDeployment.sh
@@ -43,4 +43,15 @@ cfg.members[1].priority = 1; // mongo2
 cfg.m
 '
 
+
+# mongo1 → mongo2 = 50ms, mongo1 → mongo3 = 100ms
+docker exec -it mongo1-netem sh -c "/usr/local/bin/setup-latency.sh mongo1 mongo2 50 mongo3 100"
+
+# mongo2 → mongo1 = 50ms, mongo2 → mongo3 = 75ms
+docker exec -it mongo2-netem sh -c "/usr/local/bin/setup-latency.sh mongo2 mongo1 50 mongo3 75"
+
+# mongo3 → mongo1 = 100ms, mongo3 → mongo2 = 75ms
+docker exec -it mongo3-netem sh -c "/usr/local/bin/setup-latency.sh mongo3 mongo1 100 mongo2 75"
+
+
 echo "✅ MongoDB replica set is ready with quorum writes, mongo1 preferred as primary."
