05ab83415e1459f1b67b74dfbf92e3d4a40d1c92
Ayush Pandey <8898523+ayushpandey8439@users.noreply.github.com>
2025-09-14 23:43:47 +0200

    Force janusgraph to use cassandra or fail

--- Deployments/JanusgraphReplicatedDeployment.sh
+++ Deployments/JanusgraphReplicatedDeployment.sh
@@ -8,7 +8,7 @@ docker compose -f $COMPOSE_FILE up -d
 
 # Step 1: Wait for Cassandra nodes to be ready
 echo "⏳ Waiting for Cassandra cluster to be ready..."
-for node in cassandra1 cassandra2 cassandra3; do
+for node in cassandra1; do
   until docker exec $node cqlsh -e "DESCRIBE KEYSPACES;" >/dev/null 2>&1; do
     echo "Waiting for $node..."
     sleep 5
@@ -17,21 +17,21 @@ done
 
 
 docker exec cassandra1 cqlsh -e "
-CREATE KEYSPACE IF NOT EXISTS janusgraph
-WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':3}
+CREATE KEYSPACE janusgraph
+WITH REPLICATION = {'class':'NetworkTopologyStrategy','replication_factor':1}
 "
 
 echo "✅ Cassandra cluster is ready."
 
 
 # cassandra1 → cassandra2 = 50ms, cassandra1 → cassandra3 = 100ms
-# docker exec -it cassandra1-netem sh -c "/usr/local/bin/setup-latency.sh cassandra1 cassandra2 50 cassandra3 100"
+# docker exec -it cassandra1-netem sh -c "/usr/local/bin/setup-latency.sh cassandra1 cassandra2 50 cassandra3 50"
 
 # cassandra2 → cassandra1 = 50ms, cassandra2 → cassandra3 = 75ms
-docker exec -it cassandra2-netem sh -c "/usr/local/bin/setup-latency.sh cassandra2 cassandra1 100 cassandra3 150"
+docker exec -it cassandra2-netem sh -c "/usr/local/bin/setup-latency.sh cassandra2 cassandra1 50 cassandra3 75"
 
 # cassandra3 → cassandra1 = 100ms, cassandra3 → cassandra2 = 75ms
-docker exec -it cassandra3-netem sh -c "/usr/local/bin/setup-latency.sh cassandra3 cassandra1 200 cassandra2 150"
+docker exec -it cassandra3-netem sh -c "/usr/local/bin/setup-latency.sh cassandra3 cassandra1 100 cassandra2 75"
 
 
 
