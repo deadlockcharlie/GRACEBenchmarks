b4265f52f68ee500a2438c3e89b16af140891f83
Ayush Pandey <8898523+ayushpandey8439@users.noreply.github.com>
2025-09-29 11:58:57 +0200

    Update arangoimport sequence

--- PreloadData/arangoDBImport.sh
+++ PreloadData/arangoDBImport.sh
@@ -1,5 +1,8 @@
 echo "Importing data from CSVs into arangodb"
 
-arangoimport --server.endpoint "${1}" --server.database "ycsb" --file "/var/lib/arangodb/import/vertices.csv" --type csv --collection "vertices" --create-collection true --overwrite true --create-database true --translate "_id=_key" --server.authentication false
+arangosh --server.endpoint "${1}" --server.authentication false --javascript.execute-string "db._createDatabase('ycsb');"
+arangosh --server.endpoint "${1}" --server.authentication false --javascript.execute-string "db._useDatabase('ycsb'); db._createDocumentCollection('vertices', {replicationFactor: ${2}, writeConcern: 2}); db._createEdgeCollection('edges', {replicationFactor: ${2}, writeConcern: 2});"
 
-arangoimport --server.endpoint "${1}" --server.database "ycsb" --file "/var/lib/arangodb/import/edges.csv" --type csv --collection "edges" --create-collection-type "edge" --create-collection true --overwrite true --create-database true --translate "_outV=_from" --translate "_inV=_to" --translate "_id=id" --from-collection-prefix "vertices" --to-collection-prefix "vertices" --server.authentication false
+arangoimport --server.endpoint "${1}" --server.database "ycsb" --file "/var/lib/arangodb/import/vertices.csv" --type csv --collection "vertices"  --overwrite true  --translate "_id=_key" --server.authentication false
+
+arangoimport --server.endpoint "${1}" --server.database "ycsb" --file "/var/lib/arangodb/import/edges.csv" --type csv --collection "edges" --create-collection-type "edge"  --overwrite true  --from-collection-prefix "vertices" --to-collection-prefix "vertices" --server.authentication false --translate "_id=_key" --translate "_outV=_from" --translate "_inV=_to" --translate "_id=id" 
