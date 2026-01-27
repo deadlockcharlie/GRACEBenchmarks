42142e2a34bc6fb0e245c08f3517bd7ed0f1d2b6
Ayush Pandey <8898523+ayushpandey8439@users.noreply.github.com>
2026-01-27 09:07:27 +0100

    Save single replica results

--- RunBenchmark.sh
+++ RunBenchmark.sh
@@ -113,7 +113,7 @@ DATABASES=(GRACE MemGraph Neo4j ArangoDB MongoDB JanusGraph)
 
 
 # yeast mico ldbc frbs frbm frbo frbl
-datasets=(yeast mico ldbc frbs frbm frbo)
+datasets=(yeast)
 
 
 
@@ -130,7 +130,7 @@ fi
 
 ## Replication and Latency Benchmarks
 # REPLICAS=(1 3 2 4 5 6)
-REPLICAS=(1 3)
+REPLICAS=(3)
 YCSB_THREADS=1
 for dataset in "${datasets[@]}"; do
     
